fcbd543f89a6f88d6aaa4c6bde7a0d5ce2ee6d84
mark-marshall <mark.marshallgp@gmail.com>
2019-07-10 13:45:28 +0100

    Modified mine endpoint to receive and validate proofs from the client

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -132,31 +132,31 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['POST'])
 def mine():
-    # We run the proof of work algorithm to get the next proof...
-    last_block = blockchain.last_block
-    last_proof = last_block['proof']
-    proof = blockchain.proof_of_work(last_proof)
-
-    # We must receive a reward for finding the proof.
-    # The sender is "0" to signify that this node has mine a new coin
-    # The recipient is the current node, it did the mining!
-    # The amount is 1 coin as a reward for mining the next block
-    blockchain.new_transaction( sender="0", recipient=node_identifier, amount=1)
-    # Forge the new Block by adding it to the chain
-    previous_hash = blockchain.hash(last_block)
-    block = blockchain.new_block(proof, previous_hash)
-
-
-    # Send a response with the new block
-    response = {
-        'message': "New Block Forged",
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
-    }
-    return jsonify(response), 200
-
+    proof = request.get_json()
+    # Check that the required fields are in the POST'ed data
+    required = ['proof']
+    if not proof:
+        return 'No proof sent', 400
+    else:
+        last_proof = blockchain.last_block['proof']
+        # check to see whether the new proof is valid
+        if blockchain.valid_proof(last_proof, proof):
+            # create a new transaction
+            blockchain.new_transaction( sender="0", recipient=node_identifier, amount=1)
+            previous_hash = blockchain.hash(last_block)
+            # create a new block
+            block = blockchain.new_block(proof, previous_hash)
+            # send a success response with the new block
+            response = {
+                'message': "New Block Forged",
+                'index': block['index'],
+                'transactions': block['transactions'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash'],
+            }
+            return jsonify(response), 200
+        # send a failure message
+        return 'Failure', 400
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
