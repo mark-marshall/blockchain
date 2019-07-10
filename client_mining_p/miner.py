6ef09c680daaca800254ccf8de85b0918ce3b46b
mark-marshall <mark.marshallgp@gmail.com>
2019-07-10 14:22:30 +0100

    Added client-side proof-searching and submission to server

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -6,6 +6,36 @@ import sys
 
 # TODO: Implement functionality to search for a proof 
 
+@staticmethod
+def valid_proof(last_proof, proof):
+    """
+    Validates the Proof:  Does hash(last_proof, proof) contain 4
+    leading zeroes?
+    """
+    # encode a guess
+    guess = f"{last_proof}{proof}".encode()
+    # hashing the guess
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    # return True if the last 4 digits of the hash ar zreos
+    return guess_hash[-4:] == "0000"
+
+def proof_of_work(last_proof):
+    """
+    Simple Proof of Work Algorithm
+    - Find a number p' such that hash(pp') contains 4 leading
+    zeroes, where p is the previous p'
+    - p is the previous proof, and p' is the new proof
+    """
+    # start our proof at zero
+    proof = 0
+
+    # increment proof by 1 until valid proof returns true
+    while valid_proof(last_proof, proof) is False:
+        proof += 1
+
+    # once a valid proof is reached return it
+    return proof
 
 if __name__ == '__main__':
     # What node are we interacting with?
@@ -16,12 +46,19 @@ if __name__ == '__main__':
 
     coins_mined = 0
     # Run forever until interrupted
-    while True:  
-        req = requests.get(f"{node}/last-proof")
-        last_proof = req.json()['last_proof']
-        # TODO: Get the last proof from the server and look for a new one
-        # TODO: When found, POST it to the server {"proof": new_proof}
-        # TODO: If the server responds with 'New Block Forged'
-        # add 1 to the number of coins mined and print it.  Otherwise,
-        # print the message from the server.
-        pass
+    while True:
+        # Get the last proof from the server
+        res = requests.get(f"{node}/last-proof")
+        last_proof = res.json()['last_proof']
+        # Find a new proof
+        new_proof = proof_of_work(last_proof)
+        # Post the proof to the server
+        proof_submission = requests.post(f"{node}/mine", {"proof": new_proof})
+        proof_res = proof_submission.json()
+        if proof_res['message'] == 'New Block Forged':
+            # Add 1 to the number of coins mined and print it
+            coins_mined += 1
+            print(f"Coins mined: {coins_mined}")
+        # Otherwise, print the failure message from the server
+        else:
+            print(proof_res)
